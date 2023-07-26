ad23108f97b693315fbecde721d642ae8b2c0153
Mpiloenhle thandolwethu Ntanda <125492323+Codedeveloper-MT@users.noreply.github.com>
2023-07-26 22:06:30 +0200

    Add files via upload

---
+++ Learning_b.java
@@ -0,0 +1,21 @@
+//using Scanner class
+//OUTPUT
+import java.util.Scanner;
+public class Learning_b{
+	public static void main(String[]args){
+		Scanner input=new Scanner(System.in);
+		
+		String sName,sSurname;
+		
+		System.out.println("what is your name?");
+		sName=input.nextLine();
+	
+		System.out.println("what is your Surname?");
+		sSurname=input.nextLine();
+		
+		System.out.println("your detrails is:"+ sName+" "+sSurname);
+		
+		
+
+		}
+}
